66d52a79566ae37261ec718601cf9b6270c7a424
Michael <enjoyweaver@gmail.com>
2023-11-25 13:54:42 -0600

    main

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -26,57 +26,77 @@ export default function Navbar() {
             </button>
             {showThemes && (
               <div className="absolute z-10 top-10 right-0 bg-white border border-gray-300 rounded-md shadow-lg">
-
                 <button
                   className="block w-full py-2 text-left px-4 hover:bg-gray-200"
+                  style={{ backgroundColor: '#ECF4D6', color: '#265073' }}
+                  onClick={() => selectTheme('home')}
+                >
+                  Home
+                </button>
+                <button
+                  className="block w-full py-2 text-left px-4 hover:bg-gray-200"
+                  style={{ backgroundColor: '#ffef96', color: '#0d8f4e' }}
                   onClick={() => selectTheme('lemonade')}
                 >
                   Lemonade
                 </button>
                 <button
                   className="block w-full py-2 text-left px-4 hover:bg-gray-200"
+                  style={{ backgroundColor: '#EEF5FF', color: '#70a1ff' }}
                   onClick={() => selectTheme('calm')}
                 >
                   Calm
                 </button>
                 <button
                   className="block w-full py-2 text-left px-4 hover:bg-gray-200"
+                  style={{
+                    backgroundColor: '#a4e2c6',
+                    color: '#70a1ff',
+                  }}
                   onClick={() => selectTheme('summer')}
                 >
                   Summer
                 </button>
                 <button
                   className="block w-full py-2 text-left px-4 hover:bg-gray-200"
+                  style={{
+                    backgroundColor: '#f0f5ff',
+                    color: '#e0ecff',
+                  }}
                   onClick={() => selectTheme('winter')}
                 >
                   Winter
                 </button>
                 <button
                   className="block w-full py-2 text-left px-4 hover:bg-gray-200"
+                  style={{
+                    backgroundColor: 'linear-gradient(260deg, #023E8A, #0077B6)',
+                    color: '#DBF9FF',
+                  }}
                   onClick={() => selectTheme('ocean-city')}
                 >
                   Ocean City
                 </button>
                 <button
                   className="block w-full py-2 text-left px-4 hover:bg-gray-200"
+                  style={{
+                    backgroundColor: 'linear-gradient(259deg, #e4ddd0, #887c68, #8d8479)',
+                    color: '#8d8479',
+                  }}
                   onClick={() => selectTheme('coffee')}
                 >
                   Coffee
                 </button>
                 <button
                   className="block w-full py-2 text-left px-4 hover:bg-gray-200"
-                  onClick={() => selectTheme('mocha')}
-                >
-                  Mocha
-                </button>
-
-                <button
-                  className="block w-full py-2 text-left px-4 hover:bg-gray-200"
+                  style={{
+                    backgroundColor: 'linear-gradient(260deg, #0A043C, #243B55)',
+                    color: '#FFB400',
+                  }}
                   onClick={() => selectTheme('styling')}
                 >
                   Styling
                 </button>
-                {/* New themes added */}
                 <button
                   className="block w-full py-2 text-left px-4 hover:bg-gray-200"
                   onClick={() => selectTheme('halloween')}
@@ -106,13 +126,22 @@ export default function Navbar() {
             )}
           </div>
           <div>
-            <NavLink className="mr-5" to="/">
-              Home
+
+            <NavLink className="mr-10" to="/" activeClassName="active-link">
+              <span style={{ color: 'var(--nav1)' }}>Home</span>
             </NavLink>
-            <NavLink className="mr-5" to="/about">
-              Blockchain Regulation Matrix
+            <NavLink className="mr-10" to="/about" activeClassName="active-link">
+              <span style={{ color: 'var(--nav2)' }}>Blockchain Regulation Matrix</span>
             </NavLink>
-            <NavLink to="/contact">Contact</NavLink>
+            <NavLink className="mr-10" to="/contact" activeClassName="active-link">
+              <span style={{ color: 'var(--nav3)' }}>Reducing Bureaucratic Friction</span>
+            </NavLink>
+            <NavLink className="mr-10" to="/contribute" activeClassName="active-link">
+              <span style={{ color: 'var(--nav4)' }}>Join and Contribute</span>
+            </NavLink>
+            <NavLink className="mr-10" to="/futureTopics" activeClassName="active-link">
+              <span style={{ color: 'var(--nav5)' }}>Future Topics</span>
+            </NavLink>
           </div>
         </nav>
       </header>
